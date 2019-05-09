a27500b7c49764bdbfbd4c5a700eba1a40526978
Siyuan Huang <uyhsp@student.kit.edu>
2019-05-09 13:48:34 +0200

    Signal based on the output_path

--- sick_safetyscanners/src/signal.cpp
+++ sick_safetyscanners/src/signal.cpp
@@ -13,16 +13,20 @@ int Samples = 720;
 // The Sensor angle range is from -2/3 PI to 2/3PI, and the angle increment is  0.00672220578417 Deg
 //std::vector<float> old_range = std::vector<float>(Samples, 0);
 //std::vector<float> range = std::vector<float>(Samples, 0); 
-std::vector<unsigned char> status_now = std::vector<unsigned char>(20); 
+vector<unsigned char> status_now = vector<unsigned char>(20); 
 
 void scanValues(const sick_safetyscanners::OutputPathsMsg::ConstPtr& output_paths)
 {   
     /*The Laser has 720 Samples */
     status_now = output_paths->status;
     //cout<<status_now[1]<<endl;
-    if(status_now[0]==false) cout<<"Dangours"<<endl;
+    if(status_now[0]==false) 
+    {
+        cout<<"Dangerous"<<endl;
+        //cout<<status_now[0]<<endl;
+    }
     else if(status_now[1]==false) cout<<"Critial"<<endl;
-    else cout<<"Safety"<<endl;
+    //else cout<<"Safety"<<endl;
 }
 
 int main(int argc, char **argv)
@@ -30,7 +34,6 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "static");
   ros::NodeHandle n;
   ros::Subscriber sub;
-  //sub = n.subscribe("/mybot/laser/scan", 50, scanValues);
   sub = n.subscribe("/sick_safetyscanners/output_paths", 50, scanValues);
   
   
